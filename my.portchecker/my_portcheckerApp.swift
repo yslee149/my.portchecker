2a4f12a1435c18f6a3587f8fc99941262ae9e4a0
이윤성 <yslee149@gmail.com>
2026-02-06 13:12:15 +0900

    Initial Commit

---
+++ my.portchecker/my_portcheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_portcheckerApp.swift
+//  my.portchecker
+//
+//  Created by yslee on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct my_portcheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my.portcheckerTests/my_portcheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_portcheckerTests.swift
+//  my.portcheckerTests
+//
+//  Created by yslee on 2/6/26.
+//
+
+import Testing
+@testable import my_portchecker
+
+struct my_portcheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
